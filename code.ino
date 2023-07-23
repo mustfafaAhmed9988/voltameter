8ccea20bfb8fd7b94ae24a27160707ebbecb1383
Mustafa ahmed <140299187+mustfafaAhmed9988@users.noreply.github.com>
2023-07-23 15:24:58 +0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,22 +1,16 @@
-#include <LiquidCrystal.h>
 
-const int VoltMeter = A2; 
-LiquidCrystal lcd(13,7,5,4,3,2);
+#include <LiquidCrystal.h>
+const int VoltMeter = A0; 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 float V = 0.00; 
 float R1 = 100000.00; 
 float R2 = 10000.00; 
-float vArduinoMax = 5.00; 
+float vArduinoMax = 5; 
   float resistanceRatio =   resistanceRatio = (R2 / (R1 + R2)); 
   float averageValue = 0.00; 
   float voltage = 0.00; 
 void calculate_voltage() { 
-  for (int i = 0; i < 50 ; i++) 
-  { 
-    averageValue = averageValue + analogRead(VoltMeter);
-    delay(3); 
-  } 
-  averageValue = averageValue / 50; 
-  voltage = ((averageValue * vArduinoMax) / 1024); 
+  voltage = ((analogRead(VoltMeter) * vArduinoMax) / 1024); 
   V = voltage / resistanceRatio; 
  
 } 
